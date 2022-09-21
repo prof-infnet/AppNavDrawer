9bc4e60e4814c2fd4be904d7a22f212788886021
jonh.carvalho <jonh.carvalho@prof.infnet.edu.br>
2022-09-21 10:03:15 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/infnet/appnavdrawer/MainActivity.kt
@@ -0,0 +1,32 @@
+package br.edu.infnet.appnavdrawer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var drawerLayout :DrawerLayout
+    lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById(R.id.my_drawer_layout)
+        actionBarDrawerToggle = ActionBarDrawerToggle(this, drawerLayout,R.string.nav_open, R.string.nav_close)
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (actionBarDrawerToggle.onOptionsItemSelected(item)){
+            true
+            } else super.onOptionsItemSelected(item)
+
+    }
+}
